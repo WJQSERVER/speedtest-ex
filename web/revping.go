12e21513d5224ecfb0d1b2fd9dae85416c3aaf06
WJQSERVER <wjqserver@outlook.com>
2025-01-05 15:53:45 +0800

    updat revping

--- web/revping.go
+++ web/revping.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"net/http"
+	"speedtest/config"
 
 	"github.com/gin-gonic/gin"
 	ping "github.com/prometheus-community/pro-bing"
@@ -14,35 +15,45 @@ type PingResult struct {
 	Error   string  `json:"error,omitempty"`
 }
 
-func pingIP(c *gin.Context) {
+func pingIP(c *gin.Context, cfg *config.Config) {
 	ip := c.ClientIP() // 获取客户端 IP 地址
 	if ip == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "IP address is required"})
 		return
 	}
 
-	pinger, err := ping.NewPinger(ip)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, PingResult{IP: ip, Success: false, Error: err.Error()})
-		return
+	if cfg.RevPing.Enable {
+
+		pinger, err := ping.NewPinger(ip)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, PingResult{IP: ip, Success: false, Error: err.Error()})
+			return
+		}
+
+		pinger.Count = 1 // 只发送一次 ping
+		err = pinger.Run()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, PingResult{IP: ip, Success: false, Error: err.Error()})
+			return
+		}
+
+		stats := pinger.Statistics()
+		result := PingResult{
+			IP:      ip,
+			Success: true,
+			RTT:     stats.AvgRtt.Seconds() * 1000, // 转换为毫秒
+		}
+
+		// 日志输出
+		// logInfo("icmp ping result: %v", result) // for debug
+
+		c.JSON(http.StatusOK, result)
+	} else {
+		results := PingResult{
+			IP:      ip,
+			Success: true,
+			RTT:     0,
+		}
+		c.JSON(http.StatusOK, results)
 	}
-
-	pinger.Count = 1 // 只发送一次 ping
-	err = pinger.Run()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, PingResult{IP: ip, Success: false, Error: err.Error()})
-		return
-	}
-
-	stats := pinger.Statistics()
-	result := PingResult{
-		IP:      ip,
-		Success: true,
-		RTT:     stats.AvgRtt.Seconds() * 1000, // 转换为毫秒
-	}
-
-	// 日志输出
-	// logInfo("icmp ping result: %v", result) // for debug
-
-	c.JSON(http.StatusOK, result)
 }
