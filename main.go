6f3b448b37d9e0af3e2359b46a87c7c20692cf3b
WJQSERVER <wjqserver@outlook.com>
2025-01-09 22:00:21 +0800

    update

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ Copyright (c) 2025 WJQSERVER
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"log"
@@ -88,6 +90,71 @@ func saveNewConfig() {
 	if err != nil {
 		log.Printf("Failed to save config: %v", err)
 	}
+}
+
+func updateConfig() {
+	// 写入新配置
+	saveNewConfig()
+	// 重新加载配置
+	loadConfig()
+}
+
+func generateSecret(length int) (string, error) {
+	// 生成随机字节
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+
+	// 将随机字节编码为 Base64
+	secret := base64.RawStdEncoding.EncodeToString(bytes)
+	return secret, nil
+}
+
+func initConfig() {
+	// 初始化配置
+
+	// 端口
+	if port != 0 {
+		cfg.Server.Port = port
+	}
+
+	// 开启鉴权模式
+	if auth {
+		if user != "" && password != "" {
+			cfg.Auth.Enable = true
+			cfg.Auth.Username = user
+			cfg.Auth.Password = password
+		} else {
+			fmt.Println("User name and password must be set for auth mode")
+			return
+		}
+		if secret != "" {
+			if secret == "rand" {
+				var err error
+				secret, err = generateSecret(32)
+				if err != nil {
+					fmt.Println("Failed to generate secret key:", err)
+					return
+				}
+				fmt.Println("Generated secret key")
+				cfg.Auth.Secret = secret
+			} else if len(secret) < 8 {
+				fmt.Println("Secret key must be at least 8 characters long")
+				return
+			}
+			fmt.Println("Secret key:", secret)
+			cfg.Auth.Secret = secret
+		} else {
+			fmt.Println("Secret key must be set for auth mode")
+			return
+		}
+	}
+
+	// 保存并重载
+	updateConfig()
+
 }
 
 func setupLogger() {
